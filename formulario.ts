6dfa916c831f4011fc948296de849960ceebaceb
Jeanpierremrtnz <131571417+Jeanpierremrtnz@users.noreply.github.com>
2023-07-13 00:20:40 -0500

    Update formulario.ts

--- formulario.ts
+++ formulario.ts
@@ -1,33 +1,5 @@
 import axios from 'axios';
 
-interface Producto {
-  nombre: string;
-  precio: number;
-  stock: number;
-}
-
-interface RespuestaServidor {
-  mensaje: string;
-  exito: boolean;
-}
-
-class FormularioProducto {
-    private formulario: HTMLFormElement;
-    private productoInput: HTMLInputElement;
-    private precioInput: HTMLInputElement;
-    private stockInput: HTMLInputElement;
-  
-    constructor() {
-      this.formulario = document.getElementById('formulario') as HTMLFormElement;
-      this.productoInput = document.getElementById('producto') as HTMLInputElement;
-      this.precioInput = document.getElementById('precio') as HTMLInputElement;
-      this.stockInput = document.getElementById('stock') as HTMLInputElement;
-      
-      this.formulario.addEventListener('submit', this.procesarFormulario.bind(this));
-    }
-  
-    private procesarFormulario(event: Event) {
-      event.preventDefault();
   
       const nuevoProducto: Producto = {
         nombre: this.productoInput.value,
